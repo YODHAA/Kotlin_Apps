57ab75d3bc2c8995c290df6c354be0dd0a062012
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-08 11:32:33 +0530

    Recycler Demo Made

--- Recycler_Component/app/src/main/java/com/example/recycler_component/ProgrammingAdapter.java
+++ Recycler_Component/app/src/main/java/com/example/recycler_component/ProgrammingAdapter.java
@@ -1,35 +1,53 @@
 package com.example.recycler_component;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ProgrammingAdapter extends RecyclerView.Adapter<ProgrammingAdapter.ProgrammingViewHolder> {
 
+    private String[] data;
 
+    public ProgrammingAdapter(String[] data){
+         this.data=data;         // text data we got to show in Recycler
+    }
 
     @NonNull
     @Override
     public ProgrammingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflator = LayoutInflater.from(parent.getContext());
+        View view = inflator.inflate(R.layout.list_item_layout,parent,false);
+
+        return new ProgrammingViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProgrammingViewHolder holder, int position) {
 
+        String title=data[position];
+        holder.txtTitle.setText(title);
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.length;
     }
 
     public class ProgrammingViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView imgIcon ;
+        TextView txtTitle;
         public ProgrammingViewHolder(@NonNull View itemView) {
             super(itemView);
+            imgIcon=itemView.findViewById(R.id.imgIcon);
+            txtTitle=itemView.findViewById(R.id.txtTitle);
+
         }
     }
 
