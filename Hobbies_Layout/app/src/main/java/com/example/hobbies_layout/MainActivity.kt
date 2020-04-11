875de3a722d165acddb3e659076b474fe879b4f4
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-11 15:48:23 +0530

    Section Four - Share Content to Other App Creation

--- Hobbies_Layout/app/src/main/java/com/example/hobbies_layout/MainActivity.kt
+++ Hobbies_Layout/app/src/main/java/com/example/hobbies_layout/MainActivity.kt
@@ -31,5 +31,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnShareToOtherApps.setOnClickListener {
+
+            val message:String = etUserMessage.text.toString()
+
+            val intent=Intent()
+            intent.action=Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT,message)
+            intent.type="text/plain"
+            // for the dialog that comes up
+            startActivity(Intent.createChooser(intent,"Please Select The App To Share :  "))
+        }
+
+        btnRecyclerViewDemo.setOnClickListener {
+
+            val intent=Intent(this,HobbiesActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
