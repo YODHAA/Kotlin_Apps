de62e3fb2b5130a6197fb4d00fc3bf009369374c
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-11 16:22:11 +0530

    Section Five  - Data Class Static  Creation

---
+++ Hobbies_Layout/app/src/main/java/com/example/hobbies_layout/Model.kt
@@ -0,0 +1,27 @@
+package com.example.hobbies_layout
+
+data class Hobby(var title:String)
+
+object Supplier {
+    val hobbies= listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Running"),
+        Hobby("Dancing"),
+        Hobby("Swimming"),
+        Hobby("Swimming"),
+        Hobby("Running"),
+        Hobby("Swimming"),
+        Hobby("Dancing"),
+        Hobby("Swimming"),
+        Hobby("Running"),
+        Hobby("Swimming"),
+        Hobby("Dancing"),
+        Hobby("Swimming"),
+        Hobby("Swimming"),
+        Hobby("Running"),
+        Hobby("Swimming"),
+        Hobby("Swimming"),
+        Hobby("Dancing")
+
+    )
+}
