769ecafbc916809359d34536fc27e832ea72f3eb
Yodhaa <saurabhsingh4589@gmail.com>
2020-04-11 17:00:50 +0530

    Section Six  - Full CardView Demo Creation

--- Hobbies_Layout/app/src/main/java/com/example/hobbies_layout/HobbiesAdapter.kt
+++ Hobbies_Layout/app/src/main/java/com/example/hobbies_layout/HobbiesAdapter.kt
@@ -1,20 +1,43 @@
 package com.example.hobbies_layout
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.list_item.view.*
 
 class HobbiesAdapter(val context: Context,val hobbies:List<Hobby>): RecyclerView.Adapter<HobbiesAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
 
-        fun setData(hobby: Hobby?,position: Int) {
-            itemView.txvTitle.text=hobby!!.title
+         var currentHobby:Hobby?=null
+         var currentPosition:Int =0
+
+        init {
+            itemView.setOnClickListener {
+                Toast.makeText(context,currentHobby!!.title + " Clicked !",Toast.LENGTH_LONG).show()
+            }
+            itemView.imgShare.setOnClickListener {
+                val message:String = "My Hobby is: "+currentHobby!!.title
+
+                val intent= Intent()
+                intent.action= Intent.ACTION_SEND
+                intent.putExtra(Intent.EXTRA_TEXT,message)
+                intent.type="text/plain"
+                // for the dialog that comes up
+                context.startActivity(Intent.createChooser(intent,"Please Select The App To Share :  "))
+            }
         }
 
+        fun setData(hobby: Hobby?,position: Int) {
+            itemView.txvTitle.text=hobby!!.title
+            this.currentHobby=hobby
+            this.currentPosition=position
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
