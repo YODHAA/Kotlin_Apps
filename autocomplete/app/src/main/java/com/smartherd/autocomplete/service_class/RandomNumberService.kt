7298f6aedb04345c9b3cc7cac6e44e48461082f1
Yodhaa <saurabhsingh4589@gmail.com>
2020-05-03 13:24:18 +0530

    Radio Grp Button Code Base

---
+++ autocomplete/app/src/main/java/com/smartherd/autocomplete/service_class/RandomNumberService.kt
@@ -0,0 +1,45 @@
+package com.smartherd.autocomplete.service_class
+
+import android.app.Service
+import android.content.Intent
+import android.os.Handler
+import android.os.IBinder
+import com.smartherd.autocomplete.service_class.toast
+import java.util.*
+
+class RandomNumberService : Service() {
+
+    private lateinit var mHandler: Handler
+    private lateinit var mRunnable: Runnable
+
+    override fun onBind(intent: Intent?): IBinder? {
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // Send a notification that service is started
+        toast("Service started.")
+
+        // Do a periodic task
+        mHandler = Handler()
+        mRunnable = Runnable { showRandomNumber() }
+        mHandler.postDelayed(mRunnable, 5000)
+
+        return Service.START_STICKY
+    }
+
+    override fun onDestroy() {
+        toast("Service destroyed.")
+        mHandler.removeCallbacks(mRunnable)
+        super.onDestroy()
+    }
+
+    // Custom method to do a task
+    private fun showRandomNumber() {
+        val rand = Random()
+        val number = rand.nextInt(100)
+        toast("Random Number : $number")
+        mHandler.postDelayed(mRunnable, 5000)
+    }
+
+}
