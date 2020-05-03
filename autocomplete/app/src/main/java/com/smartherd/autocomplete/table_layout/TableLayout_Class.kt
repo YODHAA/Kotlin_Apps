547a9d73c5291b1e6b4408670d97c8182b0a1876
Yodhaa <saurabhsingh4589@gmail.com>
2020-05-03 16:10:56 +0530

    Table Layout Code Base

---
+++ autocomplete/app/src/main/java/com/smartherd/autocomplete/table_layout/TableLayout_Class.kt
@@ -0,0 +1,13 @@
+package com.smartherd.autocomplete.table_layout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.smartherd.autocomplete.R
+
+class TableLayout_Class : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout)
+
+    }
+}
