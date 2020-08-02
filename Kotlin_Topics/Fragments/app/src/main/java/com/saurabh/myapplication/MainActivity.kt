48caf0684eec2e8a91e9194f33d1312b33a1e14f
Yodhaa <saurabhsingh4589@gmail.com>
2020-08-02 23:10:36 +0530

    Session 1: Fragments Swap on Button Click

--- Kotlin_Topics/Fragments/app/src/main/java/com/saurabh/myapplication/MainActivity.kt
+++ Kotlin_Topics/Fragments/app/src/main/java/com/saurabh/myapplication/MainActivity.kt
@@ -1,12 +1,85 @@
 package com.saurabh.myapplication
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentTransaction
+import com.saurabh.myapplication.fragments.FragmentOne
+import com.saurabh.myapplication.fragments.FragmentTwo
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    var isFragmentOneLoader = true
+    val manager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setToolBar()
+        showFragmentOne()
+        btn_change.contentDescription = "Button Fragment Change"
+        btn_change.setOnClickListener {
+            if (isFragmentOneLoader) showFragmentOne()
+            else showFragmentTwo()
+        }
+    }
+
+    private fun setToolBar() {
+        toolbar?.setTitle("Fragment Toolbar")
+        toolbar?.setSubtitle("Sub-Fragment Heading")
+        toolbar?.navigationIcon = ContextCompat.getDrawable(this, R.drawable.ic_home_black_24dp)
+        toolbar?.logo = ContextCompat.getDrawable(this, R.drawable.ic_perm_identity_black_24dp)
+        toolbar?.setTitleMargin(10, 10, 10, 10)
+        val mActionBar: ActionBar? = supportActionBar
+        mActionBar?.setBackgroundDrawable(ColorDrawable(Color.WHITE))
+        toolbar?.setNavigationOnClickListener {
+            Toast.makeText(applicationContext, "Navigation icon was clicked", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun showFragmentOne() {
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentOne()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        isFragmentOneLoader = false
+    }
+
+    private fun showFragmentTwo() {
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentTwo()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        isFragmentOneLoader = true
+    }
+
+    // Menu Related code
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // menuInflater.inflate(R.menu.ellipses_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.moreVertical -> {
+                Toast.makeText(applicationContext, "click on setting", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
---
+++ Kotlin_Topics/Fragments/app/src/main/java/com/saurabh/myapplication/fragments/FragmentTwo.kt
@@ -0,0 +1,74 @@
+package com.saurabh.myapplication.fragments
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.saurabh.myapplication.R
+import kotlinx.android.synthetic.main.fragment_two.*
+
+class FragmentTwo : Fragment() {
+
+    val TAG = "Fragment Two : "
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "on Attach called")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "on Create  called")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        Log.d(TAG, "on CreateView  called")
+        return inflater!!.inflate(R.layout.fragment_two, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        fragment_two.setBackgroundColor(Color.WHITE)
+        Log.d(TAG, "on ActivityCreated  called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "on onStart  called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "on onResume  called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "on OnStop  called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "on onDestroy  called")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "on onDestryView  called")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "on onDeattach  called")
+    }
+
+}
